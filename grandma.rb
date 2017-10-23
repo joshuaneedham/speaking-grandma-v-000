5e5411d68441d20b6a857d0e27f2abb2b6e3d560
Joshua Needham <joshuabneedham@gmail.com>
2017-10-23 16:34:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif  string != "I LOVE YOU GRANDMA!" || string == string.upcase
     return "NO, NOT SINCE 1938!"
-  else
-    return "I LOVE YOU TOO PUMPKIN!"    
+  else string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
